e93c409e78d0a6f3685daeb53c3fe2b1cf8d6ec7
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-04 15:18:56 -0500

    Resolve merge conflict

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,25 +2,25 @@
 
 import argparse
 
-def find_max_profit(prices):
-<<<<<<< HEAD
-  profits = []
-  for i, price in enumerate(prices):
-    for i2 in range(i,len(prices)):
-      try:
-        profits.append(prices[i2+1]-price)
-      except IndexError:
-        pass
-  return max(profits)
-=======
-  pass
 
->>>>>>> upstream/master
+def find_max_profit(prices):
+    profits = []
+    for i, price in enumerate(prices):
+        for i2 in range(i, len(prices)):
+            try:
+                profits.append(prices[i2+1]-price)
+            except IndexError:
+                pass
+    return max(profits)
+
 
 if __name__ == '__main__':
-  # This is just some code to input accepting inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to input accepting inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
