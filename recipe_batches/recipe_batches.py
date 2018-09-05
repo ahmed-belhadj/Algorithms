e9e1048740682212873ded01eacdd6e6844dbbea
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-04 20:51:28 -0500

    optimize stock_prices_py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,18 +4,22 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
-    batches = []
-    for x, y in recipe.items():
-        try:
-            batches.append(ingredients[x]/y)
-        except KeyError:
+    min_ratio = math.inf
+    for ingredient, amount in recipe.items():
+        if ingredient not in ingredients:
             return 0
-    return int(min(batches))
+        ratio = ingredients[ingredient] // amount
+        if ratio == 0:
+            return 0
+        if ratio < min_ratio:
+            min_ratio = ratio
+
+    return min_ratio
 
 
 if __name__ == '__main__':
-    # Change the entries of these dictionaries to test
-    # your implementation with different inputs
+        # Change the entries of these dictionaries to test
+        # your implementation with different inputs
     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,14 +4,15 @@ import argparse
 
 
 def find_max_profit(prices):
-    profits = []
-    for i, price in enumerate(prices):
-        for i2 in range(i, len(prices)):
-            try:
-                profits.append(prices[i2+1]-price)
-            except IndexError:
-                pass
-    return max(profits)
+    min_price = prices[0]
+    max_profit = prices[1] - min_price
+
+    for i in range(1, len(prices)):
+        price = prices[i]
+        max_profit = max(price - min_price, max_profit)
+        min_price = min(price, min_price)
+
+    return max_profit
 
 
 if __name__ == '__main__':
