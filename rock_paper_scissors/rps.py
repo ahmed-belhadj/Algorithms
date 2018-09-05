b2d259cf1a914386a85f20dd087e0b3a39953fa1
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-04 20:48:02 -0500

    rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,12 +2,25 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass
+    outcomes = []
+    plays = ['rock', 'paper', 'scissors']
+
+    def generate_plays(rounds_left, result=[]):
+        if rounds_left == 0:
+            outcomes.append(result)
+            return
+        for play in plays:
+            generate_plays(rounds_left - 1, result + [play])
+
+    generate_plays(n, [])
+    return outcomes
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
